bae2aa6149a4268ab6a7564fab2a4073a7233a7b
Mariam Imran <MARIIMRAN@my.lonestar.edu>
2022-11-21 20:47:16 -0600

    clean up

--- HW5.cpp
+++ HW5.cpp
@@ -7,226 +7,209 @@
 using namespace std;
 
 //------------
-//class stack
+// Stack
 //------------
 
 class functions
 {
-   
-private: vector<int> stack;
+
+private: 
+	
+	vector<int> stack;
 
 public:
 
-    void add(int e) //use pushback in here
-    {
-        stack.push_back(e);
-       
-    }
-
-    int pop() // use popback here to return top element
-    {
-        if (stack.size() == 0)
-        {
-            return INT_MIN;
-        }
-
-        int i = stack.at(stack.size() - 1);
-        stack.pop_back();
-       
-
-        return i;
-    }
-
-    int top() //finding top most element
-    {
-        if (stack.size() == 0)
-        {
-            return INT_MIN;
-        }
-
-        int i = stack.at(stack.size() - 1);
-        return i;
-    }
-
-    int size() //number of elements
-    {
-        return stack.size();
-    }
-    
+	void add(int e) //use pushback in here
+	{
+		stack.push_back(e);
+	}
+	int pop() // use popback here to return top element
+	{
+		if (stack.size() == 0)
+		{
+			return INT_MIN;
+		}
+		int i = stack.at(stack.size() - 1);
+		stack.pop_back();
+		return i;
+	}
+	int top() //finding top most element
+	{
+		if (stack.size() == 0)
+		{
+			return INT_MIN;
+		}
+		int i = stack.at(stack.size() - 1);
+		return i;
+	}
+	int size() //number of elements
+	{
+		return stack.size();
+	}
 };
 
 //------------
-//expressions
+// Expressions
 //------------
 
-    int priority(char c)
-    {
-        if (c == '^')
-            return 3;
-        else if (c == '/' || c == '*')
-            return 2;
-        else if (c == '+' || c == '-')
-            return 1;
-        else
-            return -1;
-    }
+int Priority(char c)
+{
+	if (c == '^')
+		return 3;
+	else if (c == '/' || c == '*')
+		return 2;
+	else if (c == '+' || c == '-')
+		return 1;
+	else
+		return -1;
+}
 
-    string infixToPostfix(string expressions)
-    {
-        functions stack; 
-        string PostFix; 
+//------------
+// Postfix
+//------------
 
-        for (int i = 0; i < expressions.size(); i++)
-        {
+string InfixToPostfix(string expressions)
+{
+	functions stack;
+	string postFix;
 
-            if (expressions[i] >= '0' && expressions[i] <= '9') //if operand we add to the output string
-            {
-                PostFix += expressions[i];
-            }
+	for (int i = 0; i < expressions.size(); i++)
+	{
+		if (expressions[i] >= '0' && expressions[i] <= '9') //if operand we add to the output string
+		{
+			postFix += expressions[i];
+		}
+		else if (expressions[i] == '(') // if '(' add to the stack
+		{
+			stack.add('(');
+		}
+		else if (expressions[i] == ')') // if ')' add to the stack, but if there is '(' then they cancel and any character between '(' and ')' will be moved to output string
+		{
+			while (stack.top() != '(') {
+				postFix += stack.top();
+				stack.pop();
+			}
+			stack.pop();
+		}
+		else
+		{
+			while (stack.size() != 0 && Priority(expressions[i]) <= Priority(stack.top())) //adding charcaters from stack to postfix
+			{
+				postFix += stack.top();
+				stack.pop();
+			}
+			stack.add(expressions[i]);
+		}
+	}
+	while (stack.size() != 0) //adding from stack to postfix
+	{
+		postFix += stack.top();
+		stack.pop();
+	}
+	return postFix;
+}
 
-            else if (expressions[i] == '(') // if '(' add to the stack
-            {
-                stack.add('(');
-            }
-
-            else if (expressions[i] == ')') // if ')' add to the stack, but if there is '(' then they cancel and any character between '(' and ')' will be moved to output string
-            {
-                while (stack.top() != '(') {
-                    PostFix += stack.top();
-                    stack.pop();
-                }
-                stack.pop();
-            }
-
-            else
-            {
-                while (stack.size() != 0 && priority(expressions[i]) <= priority(stack.top())) //adding charcaters from stack to postfix
-                {
-                    PostFix += stack.top();
-                    stack.pop();
-                }
-                stack.add(expressions[i]);
-            }
-        }
-
-        while (stack.size() != 0) //adding from stack to postfix
-        {
-            PostFix += stack.top();
-            stack.pop();
-        }
-
-        return PostFix;
-    }
-    
 //----------------
-//evaluation
+// Evaluation
 //----------------
-  
-    int Evaluate(string evaluation)
-    {
-        functions stack;
 
-        for (int i = 0; i < evaluation.size(); i++) //goes through string of operands and characters
-        {
-            if (evaluation[i] >= '0' && evaluation[i] <= '9') //checks if postfix expressions is correct
-            {
-                stack.add(evaluation[i] - '0');
-            }
-            else
-            {
-                // top two numbers from stack are popped and we then perform operations
-                if (stack.size() == 0)
-                {
-                    return INT_MIN;
-                }
-                int first = stack.pop();
+int Evaluate(string evaluation)
+{
+	functions stack;
 
-                if (stack.size() == 0)
-                {
-                    return INT_MIN;
-                }
-                int second = stack.pop();
+	for (int i = 0; i < evaluation.size(); i++) //goes through string of operands and characters
+	{
+		if (evaluation[i] >= '0' && evaluation[i] <= '9') //checks if postfix expressions is correct
+		{
+			stack.add(evaluation[i] - '0');
+		}
+		else
+		{
+			// top two numbers from stack are popped and we then perform operations
+			if (stack.size() == 0)
+			{
+				return INT_MIN;
+			}
+			int first = stack.pop();
 
-                //performing operation on top two numbers 
+			if (stack.size() == 0)
+			{
+				return INT_MIN;
+			}
+			int second = stack.pop();
 
-                if (evaluation[i] == '+')
-                {
-                    stack.add(second + first);
-                }
-                else if (evaluation[i] == '-')
-                {
-                    stack.add(second - first);
-                }
-                else if (evaluation[i] == '*')
-                {
-                    stack.add(second * first);
-                }
-                else if (evaluation[i] == '/')
-                {
-                    stack.add(second / first);
-                }
-                else if (evaluation[i] == '^')
-                {
-                    stack.add(pow(second, first));
-                }                        
-            }  
-        }
+			//performing operation on top two numbers 
 
-        int result = stack.pop();
+			if (evaluation[i] == '+')
+			{
+				stack.add(second + first);
+			}
+			else if (evaluation[i] == '-')
+			{
+				stack.add(second - first);
+			}
+			else if (evaluation[i] == '*')
+			{
+				stack.add(second * first);
+			}
+			else if (evaluation[i] == '/')
+			{
+				stack.add(second / first);
+			}
+			else if (evaluation[i] == '^')
+			{
+				stack.add(pow(second, first));
+			}
+		}
+	}
+	int result = stack.pop();
+	if (stack.size() != 0)
+	{
+		return INT_MIN;
+	}
+	return result;
+}
 
-        if (stack.size() != 0)
-        {
-            return INT_MIN;
-        }
+int main(int argc, char** argv)
+{
+	char* programme = argv[0];
+	char* part = argv[1];
+	string input = argv[2];
+	char* filename = argv[3];
 
-        return result;
-    }
+	int _part = atoi(part);
 
-    int main(int argc, char** argv)
-    {
-     
-        char* programme = argv[0];
-        char* Part = argv[1];
-        string String = argv[2];
-        char* File = argv[3];
+	ofstream fileWrite;
+	fileWrite.open(filename);
 
-        int part = atoi(Part);
+	if (_part == 2)
+	{
+		string postfix = InfixToPostfix(input);
+		fileWrite << postfix << endl;
+	}
+	else if (_part == 3)
+	{
+		int result = Evaluate(input);
+		if (result != INT_MIN)
+		{
+			fileWrite << Evaluate(input) << ".0" << endl;
+		}
+		else if (result == INT_MIN)
+		{
+			fileWrite << "nv" << endl;
+		}
+	}
+	fileWrite.close();
+	return 0;
+}
 
-        ofstream fileWrite;
-        fileWrite.open(File);
-
-        if (part == 2)
-        {
-            string postfix = infixToPostfix(String);
-            fileWrite << postfix << endl;
-        }
-
-        else if (part == 3)
-        {
-            int result = Evaluate(String);
-            if (result != INT_MIN)
-            {
-                fileWrite << Evaluate(String) << ".0" << endl;
-            }
-            else if (result == INT_MIN)
-            {
-                
-                fileWrite << "nv" << endl;
-            }
-        }
-
-        fileWrite.close();
-        return 0;
-    }
-
-    int mainTest(int argc, char** argv)
-    {
-        string input = "((4+2)*(2+6*(6-4)))/2";
-        string postfix = infixToPostfix(input);
-        int result = Evaluate(postfix);
-        cout << "Input: " << input << endl;
-        cout << "Postfix: " << postfix << endl;
-        cout << "Output: " << result << endl;
-        return 0;
-    }
-
+int mainTest(int argc, char** argv)
+{
+	string input = "((4+2)*(2+6*(6-4)))/2";
+	string postfix = InfixToPostfix(input);
+	int result = Evaluate(postfix);
+	cout << "Input: " << input << endl;
+	cout << "Postfix: " << postfix << endl;
+	cout << "Output: " << result << endl;
+	return 0;
+}
